a7a6f2ccb4db9a3e99415e3d7fef66ddc1ab0f8a
LuanFernando <luan1083@hotmail.com>
2020-09-27 17:02:06 -0300

    Imagem Logo

--- lib/app/pages/Splash/splash_screen.dart
+++ lib/app/pages/Splash/splash_screen.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Splash_Screen extends StatelessWidget {
+class Splash_Screen extends StatefulWidget {
+  @override
+  _Splash_ScreenState createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //Bloqueando a tela para não virar na horizontal
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.green[300],
-        body: Center(
-          child: Container(
-            child: Text(
-              "Favors",
-              style: TextStyle(
-                fontSize: 43.0,
-                color: Colors.white,
-              ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.asset(
+              "imagens/new-logo.png",
+              fit: BoxFit.fill,
             ),
           ),
-        ),
+        ],
       ),
     );
   }
